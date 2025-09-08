1de22bd5c53fd54b05de036e6e9710b3fae7c950
Akshay Naphade <81831871+akshaynaphade@users.noreply.github.com>
2025-09-08 22:41:35 +0530

    routes updated

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API = axios.create({ baseURL: process.env.REACT_APP_API || 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: process.env.REACT_APP_API || 'https://modern-ecommerce-web-application.onrender.com/api' });
 API.interceptors.request.use(cfg => {
   const token = localStorage.getItem('token');
   if (token) cfg.headers.Authorization = `Bearer ${token}`;
